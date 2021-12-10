092ad240c62ab005fb15dc7494cacfe24d4f6021
Zero <node.zerocool@gmail.com>
2021-12-10 00:25:03 -0600

    func ex

--- input_output.exs
+++ input_output.exs
@@ -6,3 +6,30 @@ defmodule Reader do
 end
 
 IO.puts(Reader.run())
+
+# defmodule Math do
+#   def sum(a, b) do
+#     a + b
+#   end
+# end
+#
+# IO.puts Math.sum(1, 2)
+
+defmodule Maths do
+	def sum(a, b) do
+		a + b
+	end
+end
+
+IO.puts Maths.sum(234, 23423)
+
+# Causes an error
+defp do_sum(a, b) do
+	a + b
+end
+
+IO.puts Math.do_sum(1, 2)
+
+name = IO.gets "What is your name? "
+age = IO.gets "And what is your age? "
+IO.puts "Hello, #{String.trim(name)}! You're #{String.trim(age)}? That's so old!"
