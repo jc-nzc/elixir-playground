c0cee5a50aff1d0973d48061885160f38fe39958
Zero <node.zerocool@gmail.com>
2021-12-10 22:54:14 -0600

    create fun

--- input_output.exs
+++ input_output.exs
@@ -1,35 +1,43 @@
-defmodule Reader do
-	def run do
-		"numbers.txt"
-		|> File.read!()
-	end
-end
-
-IO.puts(Reader.run())
-
-# defmodule Math do
-#   def sum(a, b) do
-#     a + b
-#   end
+# defmodule Reader do
+# 	def run do
+# 		"numbers.txt"
+# 		|> File.read!()
+# 	end
 # end
 #
-# IO.puts Math.sum(1, 2)
+# IO.puts(Reader.run())
+#
+# # defmodule Math do
+# #   def sum(a, b) do
+# #     a + b
+# #   end
+# # end
+# #
+# # IO.puts Math.sum(1, 2)
+#
+# defmodule Maths do
+# 	def sum(a, b) do
+# 		a + b
+# 	end
+# end
+#
+# IO.puts Maths.sum(234, 23423)
+#
+# # Causes an error
+# # defp do_sum(a, b) do
+# # 	a + b
+# # end
+# #
+# # IO.puts Math.do_sum(1, 2)
+#
+# name = IO.gets "What is your name? "
+# age = IO.gets "And what is your age? "
+# IO.puts "Hello, #{String.trim(name)}! You're #{String.trim(age)}? That's so old!"
 
-defmodule Maths do
-	def sum(a, b) do
-		a + b
+defmodule PipePlay do
+	def perform do
+    IO.puts "Sanity Check"
 	end
 end
 
-IO.puts Maths.sum(234, 23423)
-
-# Causes an error
-defp do_sum(a, b) do
-	a + b
-end
-
-IO.puts Math.do_sum(1, 2)
-
-name = IO.gets "What is your name? "
-age = IO.gets "And what is your age? "
-IO.puts "Hello, #{String.trim(name)}! You're #{String.trim(age)}? That's so old!"
+PipePlay.perform()
