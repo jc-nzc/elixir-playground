49551386032cc658129c226f54947d4de0af11fd
Zero <node.zerocool@gmail.com>
2021-12-31 23:52:28 -0600

    print path

--- io.exs
+++ io.exs
@@ -11,3 +11,4 @@ IO.inspect(read_file)
 # What may be happening is that you are getting a JSON response out from the get_response() function, and later on treat it as a string, while it is Map.
 # I usually get this error message when I try to use IO.puts instead of IO.inspect to print something out to console that is not a string.
 # https://elixirforum.com/t/protocol-string-chars-not-implemented-for/511/2
+IO.puts(Path.expand("~/hello"))
