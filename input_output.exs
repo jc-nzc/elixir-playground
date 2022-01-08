e22c701d31246212f109291e7c01b2118374f7c8
Zero <node.zerocool@gmail.com>
2022-01-07 21:54:25 -0600

    reduce, map algos

--- input_output.exs
+++ input_output.exs
@@ -122,3 +122,15 @@ defmodule Recursion do
 end
 
 Recursion.print_multiple_times("Hello!", 3)
+
+defmodule Math do
+	def sum_list([head | tail], accumulator) do
+		sum_list(tail, head + accumulator)
+	end
+
+	def sum_list([], accumulator) do
+		accumulator
+	end
+end
+
+IO.puts Math.sum_list([1, 2, 3, 18], 0) #=> 24
